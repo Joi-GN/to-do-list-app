88595056e76e00c750a98107259c526197afb8d0
Joice Gomes <devjoicegomes@gmail.com>
2023-01-03 22:13:06 -0300

    create DAO classes

---
+++ src/main/java/br/todo/DAO/ProjectDAO.java
@@ -0,0 +1,116 @@
+package br.todo.DAO;
+
+import br.todo.model.Project;
+import br.todo.util.ConnectionFactory;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author joice
+ */
+public class ProjectDAO {
+    
+    public void save(Project project) {
+        String sql = "INSERT INTO projects (name, description, createdAt, updatedAt) VALUES (?, ?, ?, ?);";
+        
+        Connection c = null;
+        PreparedStatement s = null;
+        
+        try {
+            c = ConnectionFactory.getConnection();
+            s = c.prepareStatement(sql);
+            s.setString(1, project.getName());
+            s.setString(2, project.getDescription());
+            s.setObject(3, project.getCreatedAt());
+            s.setObject(4, project.getUpdatedAt());
+            s.execute();
+        } catch (SQLException e) {
+            throw new RuntimeException("Erro ao salvar o projeto", e);
+        } finally {
+            ConnectionFactory.closeConnection(c, s);
+        }
+        
+    }
+    
+    public void update(Project project) {
+        String sql = "UPDATE projects SET name = ?, description = ?, updatedAt = ? WHERE id = ?;";
+        
+        Connection c = null;
+        PreparedStatement s = null;
+        
+        try {
+            c = ConnectionFactory.getConnection();
+            s = c.prepareStatement(sql);
+            s.setString(1, project.getName());
+            s.setString(2, project.getDescription());
+            s.setObject(3, project.getUpdatedAt());
+            s.setInt(4, project.getId());
+            s.execute();
+        } catch (SQLException e) {
+            throw new RuntimeException("Erro ao atualizar o projeto", e);
+        } finally {
+            ConnectionFactory.closeConnection(c, s);
+        }
+    }
+    
+    public void removeById(int projectId) {
+        String sql = "DELETE FROM projects WHERE id = ?;";
+        
+        Connection c = null;
+        PreparedStatement s = null;
+        
+        try {
+            c = ConnectionFactory.getConnection();
+            s = c.prepareStatement(sql);
+            s.setInt(1, projectId);
+            s.execute();
+        } catch (SQLException e) {
+            throw new RuntimeException("Erro ao deletar o projeto", e);
+        } finally {
+            ConnectionFactory.closeConnection(c, s);
+        }
+    }
+    
+    public List<Project> getAllProjects() {
+        List<Project> projects = new ArrayList<>();
+        String sql = "SELECT * FROM projects;";
+        
+        Connection c = null;
+        PreparedStatement s = null;
+        
+        try {
+            c = ConnectionFactory.getConnection();
+            s = c.prepareStatement(sql);
+            s.execute();
+            turnResultSetIntoList(projects, s);
+        } catch (SQLException e) {
+            throw new RuntimeException("Erro ao acessar projetos", e);
+        } finally {
+            ConnectionFactory.closeConnection(c, s);
+        }
+        return projects;
+    }
+    
+    public void turnResultSetIntoList(List<Project> list, PreparedStatement s) {
+        try(ResultSet r = s.getResultSet()) {
+            while (r.next()) {
+                Project project = new Project(
+                        r.getInt("id"), 
+                        r.getString("name"),
+                        r.getString("description"),
+                        r.getObject("createdAt", LocalDateTime.class), 
+                        r.getObject("updatedAt", LocalDateTime.class));
+                list.add(project);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException("Erro ao buscar resultado do banco de dados", e);
+        }
+    }
+    
+}
---
+++ src/main/java/br/todo/DAO/TaskDAO.java
@@ -0,0 +1,131 @@
+package br.todo.DAO;
+
+import br.todo.model.Task;
+import br.todo.util.ConnectionFactory;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author joice
+ */
+public class TaskDAO {
+    
+    public void save(Task task) {
+        String sql = "INSERT INTO tasks (idProject, name, description, notes, "
+                + "completed, deadline, createdAt, updatedAt) "
+                + "VALUES (?, ?, ?, ?, ?, ?, ?, ?);";
+
+        Connection c = null;
+        PreparedStatement s = null;
+        
+        try {
+            c = ConnectionFactory.getConnection();
+            s = c.prepareStatement(sql);
+            s.setInt(1, task.getIdProject());
+            s.setString(2, task.getName());
+            s.setString(3, task.getDescription());
+            s.setString(4, task.getNotes());
+            s.setBoolean(5, task.isCompleted());
+            s.setObject(6, task.getDeadline());
+            s.setObject(7, task.getCreatedAt());
+            s.setObject(8, task.getUpdatedAt());
+            s.execute();
+        } catch (SQLException e) {
+            throw new RuntimeException("Erro ao salvar a tarefa", e);
+        } finally {
+            ConnectionFactory.closeConnection(c, s);
+        }
+    }
+    
+    public void update(Task task) {
+        String sql = "UPDATE tasks SET name = ?, description = ?, notes = ?, "
+                + "completed = ?, deadline = ?, updatedAt = ? WHERE id = ?;";
+        
+        Connection c = null;
+        PreparedStatement s = null;
+        
+        try {
+            c = ConnectionFactory.getConnection();
+            s = c.prepareStatement(sql);
+            s.setString(1, task.getName());
+            s.setString(2, task.getDescription());
+            s.setString(3, task.getNotes());
+            s.setBoolean(4, task.isCompleted());
+            s.setObject(5, task.getDeadline());
+            s.setObject(6, task.getUpdatedAt());
+            s.setInt(7, task.getId());
+            s.execute();
+        } catch (SQLException e) {
+            throw new RuntimeException("Erro ao atualizar a tarefa", e);
+        } finally {
+            ConnectionFactory.closeConnection(c, s);
+        }
+    }
+
+    public void removeById(Integer taskId) {
+        String sql = "DELETE FROM tasks WHERE id = ?;";
+        
+        Connection c = null;
+        PreparedStatement s = null;
+        
+        try {
+            c = ConnectionFactory.getConnection();
+            s = c.prepareStatement(sql);
+            s.setInt(1, taskId);
+            s.execute();
+        } catch (SQLException e) {
+            throw new RuntimeException("Erro ao deletar a tarefa", e);
+        } finally {
+            ConnectionFactory.closeConnection(c, s);
+        }
+    }
+    
+    public List<Task> getAllTasksFromProject(Integer idProject) {
+        List<Task> tasks = new ArrayList<>();
+        String sql = "SELECT * FROM tasks WHERE idProject = ?;";
+        
+        Connection c = null;
+        PreparedStatement s = null;
+        
+        try {
+            c = ConnectionFactory.getConnection();
+            s = c.prepareStatement(sql);
+            s.setInt(1, idProject);
+            s.execute();
+            turnResultSetIntoList(tasks, s);
+        } catch (SQLException e) {
+            throw new RuntimeException("Erro ao acessar tarefas", e);
+        } finally {
+            ConnectionFactory.closeConnection(c, s);
+        }
+        return tasks;
+    }
+    
+    public void turnResultSetIntoList(List<Task> list, PreparedStatement s) {
+        try (ResultSet r = s.getResultSet()) {
+            while (r.next()) {
+                Task task = new Task(
+                        r.getInt("id"), 
+                        r.getInt("idProject"), 
+                        r.getString("name"), 
+                        r.getString("description"),
+                        r.getBoolean("completed"),
+                        r.getString("notes"), 
+                        r.getObject("deadline", LocalDate.class),
+                        r.getObject("createdAt", LocalDateTime.class),
+                        r.getObject("updatedAt", LocalDateTime.class));
+                list.add(task);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException("Erro ao buscar resultado do banco de dados", e);
+        }
+    }
+    
+}
