78830bcefb44bbfaf10083b8a0b3ae89a616532b
Joice Gomes <devjoicegomes@gmail.com>
2023-01-03 22:28:13 -0300

    refactor: change URL, USER and PASS values

--- src/main/java/br/todo/util/ConnectionFactory.java
+++ src/main/java/br/todo/util/ConnectionFactory.java
@@ -12,9 +12,9 @@ import java.sql.SQLException;
  */
 public class ConnectionFactory {
     
-    public static final String URL = "jdbc:mysql://localhost:3306/todoApp";
-    public static final String USER = "test";
-    public static final String PASS = "senha123";
+    public static final String URL = "jdbc:mysql://localhost:3306/todo_app";
+    public static final String USER = "root";
+    public static final String PASS = "";
     
     public static Connection getConnection() {
         try {
